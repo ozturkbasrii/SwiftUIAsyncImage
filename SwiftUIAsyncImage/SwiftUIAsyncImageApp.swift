1d4ee65c8584323d9a4b2492524b2b12395cc26c
ozturkbasrii <ozturkbasrii@gmail.com>
2022-09-26 11:58:34 +0300

    SwiftUI AsyncImage

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Hasan Basri Öztürk on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
